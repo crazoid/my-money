0c4c400d5d1dc5ff459bf196dcd0fc36edd9c9ee
Fabio Palhano <crazoid@gmail.com>
2019-08-16 22:35:45 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import useGet from './useGet.js'
+
+/*
+axios
+  .get('https://mymoney-crazoid.firebaseio.com/valor.json')
+  .then(res => {
+    console.log(res.data.item)
+  })
+*/
+
+/*
+axios
+  .post('https://mymoney-crazoid.firebaseio.com/valor.json',{
+    'outro': 'Fabio Palhano'
+  })
+  .then(res => {
+    console.log(res)
+  })
+*/
+
 
 function App() {
+  const data = useGet('https://mymoney-crazoid.firebaseio.com/movimentacoes/2019-08.json')
+  const data2 = useGet('http://httpbin.org/ip')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Money</h1>
+      { JSON.stringify({data})}
+      { data.loading && <p>Loading...</p>}
+      <pre>{ JSON.stringify({data2})}</pre>
     </div>
-  );
+  )
 }
 
 export default App;
